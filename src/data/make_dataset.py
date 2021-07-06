bb2e2f48170056ffd9ab67eab50eb193aec3b53c
tvhahn <tvhahn@gmail.com>
2021-07-06 15:07:13 -0400

    minor formatting

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -5,7 +5,11 @@ from pathlib import Path
 from pathlib import Path
 import numpy as np
 import re
-from src.data.data_prep_utils import create_truth_and_downsampled_x, min_max_array, scaler
+from src.data.data_prep_utils import (
+    create_truth_and_downsampled_x,
+    min_max_array,
+    scaler,
+)
 
 
 """
@@ -38,27 +42,32 @@ About Data:
 
 #!#!#!#!#!#!#!#!# RUN SCRIPT #!#!#!#!#!#!#!#!#
 
+
 @click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('interim_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
+@click.argument("input_filepath", type=click.Path(exists=True))
+@click.argument("interim_filepath", type=click.Path(exists=True))
+@click.argument("output_filepath", type=click.Path())
 def main(input_filepath, interim_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
+    """Runs data processing scripts to turn raw data from (../raw) into
+    cleaned data ready to be analyzed (saved in ../processed).
     """
     logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
-
+    logger.info("making final data set from raw data")
 
     # folder_raw_data = root_dir / 'data/raw/' # raw data folder that holds the .nc files
-    folder_raw_data = root_dir / input_filepath # raw data folder that holds the .nc files
+    folder_raw_data = (
+        root_dir / input_filepath
+    )  # raw data folder that holds the .nc files
 
+    folder_input_data_interim = (
+        root_dir / interim_filepath / "input"
+    )  # 8x downsampled data - unscaled
+    folder_truth_data_interim = (
+        root_dir / interim_filepath / "truth"
+    )  # "original" data (60% of native file h & w) - unscaled
 
-    folder_input_data_interim = root_dir / interim_filepath / 'input' # 8x downsampled data - unscaled
-    folder_truth_data_interim = root_dir / interim_filepath/ 'truth' # "original" data (60% of native file h & w) - unscaled
-
-    folder_input_data = root_dir / output_filepath / 'input' # final data scaled
-    folder_truth_data = root_dir / output_filepath / 'truth' # final data scaled
+    folder_input_data = root_dir / output_filepath / "input"  # final data scaled
+    folder_truth_data = root_dir / output_filepath / "truth"  # final data scaled
 
     # make processed data folder if they don't exist already
     Path(folder_input_data_interim).mkdir(parents=True, exist_ok=True)
@@ -67,7 +76,7 @@ def main(input_filepath, interim_filepath, output_filepath):
     Path(folder_truth_data).mkdir(parents=True, exist_ok=True)
 
     # get a list of file names
-    file_list = list(folder_raw_data.rglob('*.nc'))
+    file_list = list(folder_raw_data.rglob("*.nc"))
 
     # instantiate min/max lists that will be used to later
     # scale the data
@@ -79,15 +88,15 @@ def main(input_filepath, interim_filepath, output_filepath):
     # create all interim data (not yet scaled)
     for i, file in enumerate(file_list):
         # get time step
-        time_step = re.findall('[0-9]+', str(file))[-1]
-        x_save_name = f'x_{time_step}.npy'
-        x_truth_save_name = f'x_truth_{time_step}.npy'
-        
+        time_step = re.findall("[0-9]+", str(file))[-1]
+        x_save_name = f"x_{time_step}.npy"
+        x_truth_save_name = f"x_truth_{time_step}.npy"
+
         x_truth, x = create_truth_and_downsampled_x(file)
 
         np.save(folder_input_data_interim / x_save_name, x)
         np.save(folder_truth_data_interim / x_truth_save_name, x_truth)
-        
+
         min_vals, max_vals = min_max_array(x)
         min_list_input.append(min_vals)
         max_list_input.append(max_vals)
@@ -96,28 +105,35 @@ def main(input_filepath, interim_filepath, output_filepath):
         min_list_truth.append(min_vals)
         max_list_truth.append(max_vals)
 
-    min_array_input = np.min(np.array(min_list_input),axis=0)
-    max_array_input = np.max(np.array(max_list_input),axis=0)
+    min_array_input = np.min(np.array(min_list_input), axis=0)
+    max_array_input = np.max(np.array(max_list_input), axis=0)
 
-    min_array_truth = np.min(np.array(min_list_truth),axis=0)
-    max_array_truth = np.max(np.array(max_list_truth),axis=0)
-
+    min_array_truth = np.min(np.array(min_list_truth), axis=0)
+    max_array_truth = np.max(np.array(max_list_truth), axis=0)
 
     # scale all data and save in processed folder
     for i, file in enumerate(file_list):
-        time_step = re.findall('[0-9]+', str(file))[-1]
-        x_save_name = f'x_{time_step}.npy'
-        x_truth_save_name = f'x_truth_{time_step}.npy'
+        time_step = re.findall("[0-9]+", str(file))[-1]
+        x_save_name = f"x_{time_step}.npy"
+        x_truth_save_name = f"x_truth_{time_step}.npy"
 
-        x = scaler(np.load(folder_input_data_interim / x_save_name), min_array_input, max_array_input)
+        x = scaler(
+            np.load(folder_input_data_interim / x_save_name),
+            min_array_input,
+            max_array_input,
+        )
         np.save(folder_input_data / x_save_name, x)
 
-        x_truth = scaler(np.load(folder_truth_data_interim / x_truth_save_name), min_array_truth, max_array_truth)
+        x_truth = scaler(
+            np.load(folder_truth_data_interim / x_truth_save_name),
+            min_array_truth,
+            max_array_truth,
+        )
         np.save(folder_truth_data / x_truth_save_name, x_truth)
 
 
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+if __name__ == "__main__":
+    log_fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
     logging.basicConfig(level=logging.INFO, format=log_fmt)
 
     # establish folders for holding raw, interim (un-scaled data), and final processed
