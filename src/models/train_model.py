c1e2f2c0a5ed2f23ba1a8f174d95356986eacd03
tvhahn <tvhahn@gmail.com>
2021-07-09 13:34:36 -0400

    updating training loop

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -9,6 +9,7 @@ from pathlib import Path
 
 from src.models.utils.create_batch import EarthDataTrain
 from src.models.model.model import Generator, Discriminator, init_weights
+from src.models.loss.wasserstein import gradient_penalty
 
 #######################################################
 # Set Hyperparameters
@@ -19,6 +20,7 @@ NUM_EPOCHS = 2
 BATCH_SIZE = 2
 CRITIC_ITERATIONS = 5
 LAMBDA_GP = 10
+GEN_PRETRAIN_EPOCHS= 5 # number of epochs to pretrain generator
 #######################################################
 
 
@@ -54,29 +56,44 @@ critic.train()
 for epoch in range(NUM_EPOCHS):
     print('epoch', epoch)
 
+
     for batch_idx, data in enumerate(loader):
         x_truth = data['truth'].to(device)
         x_up = data['upsampled'].to(device)
+        x_input = data['input'].to(device)
 
-        for _ in range(CRITIC_ITERATIONS):
-            fake = gen(x_input)
-            
-    #         loss = criterion(output, x_target)
-            
-            critic_real = critic(torch.cat([x_truth, x_up], dim=1)).view(-1)
-            critic_fake = critic(torch.cat([fake, x_up], dim=1)).view(-1)
-            
-            gp = gradient_penalty(critic, 
-                                torch.cat([x_truth, x_up], dim=1), # real
-                                torch.cat([fake, x_up], dim=1),  # fake
-                                device=device)
+        # pre-train the generator with simple MSE loss
+        if epoch < GEN_PRETRAIN_EPOCHS:
+            criterion = nn.MSELoss()
+            gen_fake = gen(x_input)
+            loss_mse = criterion(gen_fake, x_truth)
+            gen.zero_grad()
+            loss_mse.backward()
+            opt_gen.step()
 
-    #         gp = wgan_grad_penalty(critic, torch.cat([fake, x_up], dim=1), torch.cat([x_truth, x_up], dim=1))
-            
-            loss_critic = (
-                    -(torch.mean(critic_real) - torch.mean(critic_fake)) + LAMBDA_GP * gp
-                )
-            
-            critic.zero_grad()
-            loss_critic.backward(retain_graph=True)
-            opt_critic.step()
+        # after pre-training of generator, enter the 
+        # full training loop and train critic (e.g. discriminator) too
+        else:
+            # train critic
+            for _ in range(CRITIC_ITERATIONS):
+                fake = gen(x_input)
+                
+        #         loss = criterion(output, x_target)
+                
+                critic_real = critic(torch.cat([x_truth, x_up], dim=1)).view(-1)
+                critic_fake = critic(torch.cat([fake, x_up], dim=1)).view(-1)
+                
+                gp = gradient_penalty(critic, 
+                                    torch.cat([x_truth, x_up], dim=1), # real
+                                    torch.cat([fake, x_up], dim=1),  # fake
+                                    device=device)
+
+        #         gp = wgan_grad_penalty(critic, torch.cat([fake, x_up], dim=1), torch.cat([x_truth, x_up], dim=1))
+                
+                loss_critic = (
+                        -(torch.mean(critic_real) - torch.mean(critic_fake)) + LAMBDA_GP * gp
+                    )
+                
+                critic.zero_grad()
+                loss_critic.backward(retain_graph=True)
+                opt_critic.step()
