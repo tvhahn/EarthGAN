e91a823ebbd2e7e21b9908ee2774618f48d570f4
tvhahn <tvhahn@gmail.com>
2021-07-13 10:29:36 -0400

    basic model.py working. deploy on HPC next

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -4,10 +4,11 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 import torchvision
-from torch.utils.tensorboard import SummaryWriter # print to tensorboard
+from torch.utils.tensorboard import SummaryWriter  # print to tensorboard
 from torch.utils.data import DataLoader
 from pathlib import Path
 import matplotlib.pyplot as plt
+import datetime
 
 
 from src.models.utils.create_batch import EarthDataTrain
@@ -15,30 +16,34 @@ from src.models.model.model import Generator, Discriminator, init_weights
 from src.models.loss.wasserstein import gradient_penalty
 
 ########################################################
-# Helper function
+# Helper functions
+#######################################################
+
+
 def plot_fake_truth(fake, x_truth):
-    plt.switch_backend('agg')    
+    plt.switch_backend("agg")
     with torch.no_grad():
         fake = gen(x_input).cpu()
         x_truth = x_truth.cpu()
-        color_scheme='inferno'
+        color_scheme = "inferno"
         no_col = 10
-        fig, ax = plt.subplots(2,no_col, figsize=(18,8))
+        fig, ax = plt.subplots(2, no_col, figsize=(12, 6))
         b, v, r, h, w = fake.shape
         for i in range(no_col):
-            bi = torch.randint(b,(1,)).item()
-            vi = torch.randint(v,(1,)).item()
-            ri = torch.randint(r,(1,)).item()
-            ax[0,i].pcolormesh(fake[bi,vi,ri,:,:].cpu(), cmap=color_scheme)
-            ax[0,i].get_xaxis().set_visible(False)
-            ax[0,i].get_yaxis().set_visible(False)
-            ax[0,i].title.set_text(f'v={vi}, r={ri}')
-            ax[1,i].pcolormesh(x_truth[bi,vi,ri,:,:].cpu(), cmap=color_scheme)
-            ax[1,i].get_xaxis().set_visible(False)
-            ax[1,i].get_yaxis().set_visible(False)
+            bi = torch.randint(b, (1,)).item()
+            vi = torch.randint(v, (1,)).item()
+            ri = torch.randint(r, (1,)).item()
+            ax[0, i].pcolormesh(fake[bi, vi, ri, :, :].cpu(), cmap=color_scheme)
+            ax[0, i].get_xaxis().set_visible(False)
+            ax[0, i].get_yaxis().set_visible(False)
+            ax[0, i].title.set_text(f"v={vi}, r={ri}")
+            ax[1, i].pcolormesh(x_truth[bi, vi, ri, :, :].cpu(), cmap=color_scheme)
+            ax[1, i].get_xaxis().set_visible(False)
+            ax[1, i].get_yaxis().set_visible(False)
         plt.subplots_adjust(wspace=0, hspace=0)
-        
+
     return fig
+
 
 #######################################################
 # Set Hyperparameters
@@ -46,20 +51,24 @@ def plot_fake_truth(fake, x_truth):
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 LEARNING_RATE = 1e-4
-NUM_EPOCHS = 2
+NUM_EPOCHS = 20
 BATCH_SIZE = 2
 CRITIC_ITERATIONS = 5
 LAMBDA_GP = 10
-GEN_PRETRAIN_EPOCHS= 5 # number of epochs to pretrain generator
+GEN_PRETRAIN_EPOCHS = 80  # number of epochs to pretrain generator
 
 #######################################################
 
 
-root_dir = Path.cwd().parent # set the root directory as a Pathlib path
+root_dir = Path.cwd().parent.parent  # set the root directory as a Pathlib path
 print(root_dir)
 
-path_input_folder = root_dir / 'data/processed/input'
-path_truth_folder = root_dir / 'data/processed/truth'
+model_start_time = datetime.datetime.now().strftime("%Y_%m_%d_%H%M%S")
+
+path_input_folder = root_dir / "data/processed/input"
+path_truth_folder = root_dir / "data/processed/truth"
+path_checkpoint_folder = root_dir / "models/interim/checkpoints" / model_start_time
+Path(path_checkpoint_folder).mkdir(parents=True, exist_ok=True)
 
 earth_dataset = EarthDataTrain(path_input_folder, path_truth_folder)
 
@@ -70,11 +79,22 @@ loader = DataLoader(
 )
 
 # set summary writer for Tensorboard
-writer_results = SummaryWriter(f"logs/results")
-step = 0
+writer_results = SummaryWriter(root_dir / "models/interim/logs/" / model_start_time)
 
-gen = Generator(in_chan=4, out_chan=4, scale_factor=8, chan_base=32, chan_min=32, chan_max=64, cat_noise=True).to(device)
-critic = Discriminator(in_chan=8, out_chan=8, scale_factor=8, chan_base=32, chan_min=32, chan_max=64).to(device)
+
+gen = Generator(
+    in_chan=4,
+    out_chan=4,
+    scale_factor=8,
+    chan_base=32,
+    chan_min=32,
+    chan_max=64,
+    cat_noise=True,
+).to(device)
+
+critic = Discriminator(
+    in_chan=8, out_chan=8, scale_factor=8, chan_base=32, chan_min=32, chan_max=64
+).to(device)
 
 # initialize weights
 gen.apply(init_weights)
@@ -87,15 +107,14 @@ opt_critic = optim.Adam(critic.parameters(), lr=1e-4, betas=(0.0, 0.9))
 gen.train()
 critic.train()
 
-
+step = 0
 for epoch in range(NUM_EPOCHS):
-    print('epoch', epoch)
-
+    print("epoch", epoch)
 
     for batch_idx, data in enumerate(loader):
-        x_truth = data['truth'].to(device)
-        x_up = data['upsampled'].to(device)
-        x_input = data['input'].to(device)
+        x_truth = data["truth"].to(device)
+        x_up = data["upsampled"].to(device)
+        x_input = data["input"].to(device)
 
         # pre-train the generator with simple MSE loss
         if epoch < GEN_PRETRAIN_EPOCHS:
@@ -106,25 +125,28 @@ for epoch in range(NUM_EPOCHS):
             loss_mse.backward()
             opt_gen.step()
 
-        # after pre-training of generator, enter the 
+        # after pre-training of generator, enter the
         # full training loop and train critic (e.g. discriminator) too
         else:
             # train critic
             for _ in range(CRITIC_ITERATIONS):
                 fake = gen(x_input)
-                
+
                 critic_real = critic(torch.cat([x_truth, x_up], dim=1)).view(-1)
                 critic_fake = critic(torch.cat([fake, x_up], dim=1)).view(-1)
-                
-                gp = gradient_penalty(critic, 
-                                    torch.cat([x_truth, x_up], dim=1), # real
-                                    torch.cat([fake, x_up], dim=1),  # fake
-                                    device=device)
+
+                gp = gradient_penalty(
+                    critic,
+                    torch.cat([x_truth, x_up], dim=1),  # real
+                    torch.cat([fake, x_up], dim=1),  # fake
+                    device=device,
+                )
 
                 loss_critic = (
-                        -(torch.mean(critic_real) - torch.mean(critic_fake)) + LAMBDA_GP * gp
-                    )
-                
+                    -(torch.mean(critic_real) - torch.mean(critic_fake))
+                    + LAMBDA_GP * gp
+                )
+
                 critic.zero_grad()
                 loss_critic.backward(retain_graph=True)
                 opt_critic.step()
@@ -135,9 +157,22 @@ for epoch in range(NUM_EPOCHS):
             gen.zero_grad()
             loss_gen.backward()
             opt_gen.step()
-        with torch.no_grad():
-            fake = gen(x_input)
-            fig = plot_fake_truth(fake, x_truth)
-            writer_results.add_figure("Results", fig, global_step=step)
 
-        step += 1
+    with torch.no_grad():
+        fake = gen(x_input)
+        fig = plot_fake_truth(fake, x_truth)
+        writer_results.add_figure("Results", fig, global_step=step)
+
+    step += 1
+
+    # save checkpoint
+    torch.save(
+        {
+            "gen": gen.state_dict(),
+            "critic": critic.state_dict(),
+            "opt_gen": opt_gen.state_dict(),
+            "opt_critic": opt_critic.state_dict(),
+            "epoch": epoch,
+        },
+        path_checkpoint_folder / f"train_{epoch}.pt",
+    )
