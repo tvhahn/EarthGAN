8336736114434ae4716dd02205688af370c9ef5f
tvhahn <tvhahn@gmail.com>
2021-11-05 18:39:06 -0400

    update bash script for new argparse arg

--- bash_scripts/train_model_local.sh
+++ bash_scripts/train_model_local.sh
@@ -8,4 +8,16 @@ cd $PROJECT_DIR
 #   python train_model.py ./data/processed -c 2021_07_15_093623
 # train_model.py will then look for the most recent checkpoint .pt file
 # in the checkpoint folder and load the checkpoint before model training
-python $PROJECT_DIR/src/models/train_model.py ./data/processed
+python $PROJECT_DIR/src/models/train_model.py \
+    --path_data ./data/processed \
+    --proj_dir $PROJECT_DIR \
+    --batch_size 1 \
+    --var_to_include 4 \
+    --learning_rate 1e-4 \
+    --critic_iterations 5 \
+    --num_epochs 1000 \
+    --lambda_gp 10 \
+    --gen_pretrain_epochs 20 \
+    --model_time_suffix var4 \
+    --checkpoint  2021_11_04_191811_var4 \
+    # --cat_noise
