aec52fc200a28512263b0514fb16e370adceeacf
tvhahn <tvhahn@gmail.com>
2021-11-20 00:43:26 -0500

    update for latest run

--- bash_scripts/train_model_hpc.sh
+++ bash_scripts/train_model_hpc.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --account=rrg-mechefsk
-#SBATCH --gres=gpu:t4:1        # request GPU "generic resource"
+#SBATCH --gres=gpu:v100:1        # request GPU "generic resource"
 #SBATCH --cpus-per-task=4   # maximum CPU cores per GPU request: 6 on Cedar, 16 on Graham.
 #SBATCH --mem=14000M      # memory per node
 #SBATCH --time=0-23:50      # time (DD-HH:MM)
-#SBATCH --job-name=var1
+#SBATCH --job-name=var4
 #SBATCH --output=%N-%j.out  # %N for node name, %j for jobID
 #SBATCH --mail-type=ALL               # Type of email notification- BEGIN,END,F$
 #SBATCH --mail-user=18tcvh@queensu.ca   # Email to which notifications will be $
@@ -29,12 +29,12 @@ python $PROJECT_DIR/src/models/train_model.py \
     --proj_dir $PROJECT_DIR \
     --path_data $SLURM_TMPDIR/data/processed \
     --batch_size 1 \
-    --var_to_include 1 \
+    --var_to_include 4 \
     --learning_rate 1e-4 \
     --critic_iterations 5 \
     --num_epochs 1000 \
     --lambda_gp 10 \
     --gen_pretrain_epochs 1 \
-    --model_time_suffix var1 \
-    --checkpoint  2021_11_17_121145_var1 \
+    --model_time_suffix var4 \
+    --checkpoint  2021_11_18_103508_var4 \
     # --cat_noise
